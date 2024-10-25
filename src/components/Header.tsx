76f24e21b712d42adc1cbe22c4eb4762942983b1
SplashyCoder <pachecomorahectordavid@gmail.com>
2024-10-25 11:43:21 -0500

    header altmost ready and adding dark/ligth mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import MenuItem from "@/components/MenuItem";
 import { AiFillHome } from "react-icons/ai";
 import { BsFillInfoCircleFill } from "react-icons/bs";
+import Link from "next/link";
 
 const Header = () => {
   return (
     <div>
-      <nav>
-        <div className="">
+      <nav className="flex justify-between items-center p-3 max-w-6xl mx-auto">
+        <div className="flex gap-4 ">
           <MenuItem title="home" address="/" Icon={AiFillHome} />
           <MenuItem
             title="about"
@@ -15,7 +16,15 @@ const Header = () => {
             Icon={BsFillInfoCircleFill}
           />
         </div>
-        <div className=""></div>
+        <div className=" flex gap-1 items-center">
+          <Link
+            href={"/"}
+            className="text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg "
+          >
+            IMDb
+          </Link>
+          <span className="text-xl hidden sm:inline ">Clone</span>
+        </div>
       </nav>
     </div>
   );
