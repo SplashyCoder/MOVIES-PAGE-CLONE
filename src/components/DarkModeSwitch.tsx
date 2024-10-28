0babab57e6d5bff1bb639ab75eac2797b74d3aaa
SplashyCoder <pachecomorahectordavid@gmail.com>
2024-10-28 14:30:23 -0500

    Light & dark mode ready for the basic stuff

--- src/components/DarkModeSwitch.tsx
+++ src/components/DarkModeSwitch.tsx
@@ -1,26 +1,29 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MdLightMode, MdDarkMode } from "react-icons/md";
 import { useTheme } from "next-themes";
 
 const DarkModeSwitch = () => {
   const { theme, setTheme, systemTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
   const currenTheme = theme === "system" ? systemTheme : theme;
+  useEffect(() => setMounted(true), []);
 
   return (
     <div>
-      {currenTheme === "dark" ? (
-        <MdDarkMode
-          onClick={() => setTheme("ligth")}
-          className="text-xl cursor-pointer hover:text-amber-500"
-        />
-      ) : (
-        <MdLightMode
-          onClick={() => setTheme("dark")}
-          className="text-xl cursor-pointer hover:text-amber-500"
-        />
-      )}
+      {mounted &&
+        (currenTheme === "dark" ? (
+          <MdDarkMode
+            onClick={() => setTheme("ligth")}
+            className="text-xl cursor-pointer hover:text-amber-500"
+          />
+        ) : (
+          <MdLightMode
+            onClick={() => setTheme("dark")}
+            className="text-xl cursor-pointer hover:text-amber-500"
+          />
+        ))}
     </div>
   );
 };
